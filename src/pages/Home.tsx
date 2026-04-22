3ce2e58b8b0dd33e0581fdd7895ef5a56ad19001
lastoned <filipsarovic428@gmail.com>
2026-04-22 14:53:06 +0200

    setup folder structure

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+
+const Home = () => {
+  return (
+    <section className='min-h-screen px-4 max-w-md mx-auto'>
+      <header className="flex items-center justify-between py-4">
+        <h1 className="text-2xl font-semibold text-[#E53935]">
+          Kuhinjica 🍲
+        </h1>
+
+        <button className="text-xl">
+          🛒
+        </button>
+      </header>
+    </section>
+  ) 
+}
+
+export default Home
